1fab8698d1a2e83e06183a7bea97cb2cafa0ab33
Gavin <gavinvaught@gmail.com>
2016-07-28 16:42:13 -0500

    wrote a join query to show all departments and the current managers of said departments

---
+++ join_exercises.sql
@@ -0,0 +1,10 @@
+use employees;
+
+-- write a query showing each department name and the current manager of that dept.
+SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS full_name
+FROM departments AS d
+JOIN dept_manager AS dm
+ ON dm.dept_no = d.dept_no
+JOIN employees AS e
+ ON e.emp_no = dm.emp_no
+WHERE dm.to_date > curdate();
