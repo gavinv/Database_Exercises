99713be7a529e866390346370f3ccb551b04b910
Gavin <gavinvaught@gmail.com>
2016-07-28 16:48:22 -0500

    added AND statement to the WHERE clause to show only female managers

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,6 +1,6 @@
 use employees;
 
--- write a query showing each department name and the current manager of that dept.
+-- Write a query showing each department name and the current manager of that dept.
 SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS full_name
 FROM departments AS d
 JOIN dept_manager AS dm
@@ -8,3 +8,12 @@ JOIN dept_manager AS dm
 JOIN employees AS e
  ON e.emp_no = dm.emp_no
 WHERE dm.to_date > curdate();
+
+-- Find the name of all departments currently managed by women.
+SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS full_name
+FROM departments AS d
+JOIN dept_manager AS dm
+ ON dm.dept_no = d.dept_no
+JOIN employees AS e
+ ON e.emp_no = dm.emp_no
+WHERE dm.to_date > curdate() AND e.gender = 'F'
