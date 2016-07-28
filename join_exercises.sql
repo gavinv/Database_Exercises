07a127401f2312048150ee9b85c69891f8aace06
Gavin <gavinvaught@gmail.com>
2016-07-28 18:30:33 -0500

    made a query to show all the count of employees in customer service dept for each title

--- join_exercises.sql
+++ join_exercises.sql
@@ -16,4 +16,16 @@ JOIN dept_manager AS dm
  ON dm.dept_no = d.dept_no
 JOIN employees AS e
  ON e.emp_no = dm.emp_no
-WHERE dm.to_date > curdate() AND e.gender = 'F'
+WHERE dm.to_date > curdate() AND e.gender = 'F';
+
+-- Find the current titles of employees currently working in the Customer Service department.
+SELECT t.title, COUNT(e.emp_no) AS count
+FROM employees AS e
+JOIN titles AS t
+ ON t.emp_no = e.emp_no
+JOIN dept_emp AS de
+ ON de.emp_no = e.emp_no
+JOIN  departments AS d
+ ON d.dept_no = de.dept_no
+WHERE d.dept_no = 'd009' AND t.to_date > curdate()
+GROUP BY t.title;
